c583aa90ac3375b5c93fe948e6b350e8d9ef9d39
Felix Ji-13980K <feljii@outlook.com>
2026-04-23 16:46:43 +0800

    refactor: 移除废弃模型（ExtractionOptions、ExtractionTemplate、LLMConfig）

--- src/vibeocr/models/__init__.py
+++ src/vibeocr/models/__init__.py
@@ -1,16 +1,7 @@
 """OCR 数据模型"""
 
-from vibeocr.models.extraction_options import ExtractionOptions
-from vibeocr.models.extraction_template import DEFAULT_TEMPLATES, ExtractionTemplate
-from vibeocr.models.llm_config import APIType, LLMConfig, LLMConfigs
 from vibeocr.models.ocr_result import OCRResult
 
 __all__ = [
-    "DEFAULT_TEMPLATES",
-    "APIType",
-    "ExtractionOptions",
-    "ExtractionTemplate",
-    "LLMConfig",
-    "LLMConfigs",
     "OCRResult",
 ]
