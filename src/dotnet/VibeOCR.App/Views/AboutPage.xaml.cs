3bc291ad9a844e7fa8dce5d7cabff643abb03d29
Felix Ji-13980K <feljii@outlook.com>
2026-07-14 18:34:28 +0800

    refactor(winui): about page two-column cards, move shell options out

--- src/dotnet/VibeOCR.App/Views/AboutPage.xaml.cs
+++ src/dotnet/VibeOCR.App/Views/AboutPage.xaml.cs
@@ -17,15 +17,6 @@ public sealed partial class AboutPage : Page
     public ShellViewModel Shell { get; }
     public UpdateViewModel Update { get; }
 
-    private void OnApplyHotkeyClicked(object sender, RoutedEventArgs e) => Shell.ApplyHotkey();
-
-    private void OnStartupClicked(object sender, RoutedEventArgs e)
-        => Shell.SetStartWithSystem(Shell.StartWithSystem);
-
-    private void OnHideToTrayClicked(object sender, RoutedEventArgs e) => Shell.HideToTray();
-
-    private void OnQuitClicked(object sender, RoutedEventArgs e) => Shell.Quit();
-
     private async void OnCheckUpdateClicked(object sender, RoutedEventArgs e)
         => await Update.CheckAsync(CancellationToken.None);
 
